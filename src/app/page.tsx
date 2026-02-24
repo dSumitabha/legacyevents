2f1c2b3c70a22ba13aebaa7f17aedf0e8fbe6686
dSumitabha <fullstackdev@zanservices.com>
2026-02-24 18:06:19 +0530

    brand philosophy section added to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/sections/HeroSection"
 import ServiceSection from "@/components/sections/ServicesSection"
+import BrandPhilosophy from "@/components/sections/BrandPhilosophy";
 
 export default function Home() {
   return (
     <>
         <HeroSection />
         <ServiceSection />
+        <BrandPhilosophy />
     </>
   );
 }
